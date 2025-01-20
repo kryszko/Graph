aa1c22cb55107be10efadfbaaa1f8c37aaa62a96
kryszko <krypieli@gmail.com>
2025-01-20 13:08:49 +0100

    szkielet

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
 
-int main()
+int main(int ile, char * params[])
 {
+    // poprawność parametrów
+    //auto [input_file_name, output_file_name] = check_params(ile, params);
+
+    //if (not input_file_name.empty() and not output_file_name.empty())
+    //{
+    //    // działa
+    //    graf numbers = read_numbers(input_file_name);
+    //    coloring(numbers);
+    //    write_colors(output_file_name);
+    //}
+    //else
+    //{
+    //    help();
+    //}
+
     return 0;
 }
